7329a7641fbe5353d6246010ee126b1546212f5f
ZackGaming210 <44915154+ZackGaming210@users.noreply.github.com>
2026-03-18 08:43:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// --- Dummy port for Render Web Service ---
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
 import { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField } from "discord.js";
 import tmi from "tmi.js";
 import fetch from "node-fetch";
